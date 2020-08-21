f16af99998ac19cc2a450fa66beee9698cbcac81
gracenak <gracenak@gmail.com>
2020-08-20 23:33:10 -0700

    update

--- lib/gnakano_cli_project/cli.rb
+++ lib/gnakano_cli_project/cli.rb
@@ -7,66 +7,89 @@ URL = "https://arizonachambermusic.org/tickets/"
         puts "|  Welcome to Arizona Friends of Chamber Music Concerts!  |"
         puts "+---------------------------------------------------------+"
         main_menu
+        get_advertised_concerts
+        
         list_concerts
+        # get_user_concert
     end
 
     def main_menu
         puts ""
         puts "To see a list of concerts, type 'list'"
         input = gets.strip
-        list_concerts
 
         puts ""
-        puts "To find out more information about a specific concert, enter the number"
-        input = gets.strip.to_i
-        print_concerts(concert)
-
-        puts "Would you like to check out another concert, type 'y' or 'n'?"
-        input = gets.strip.downcase
-        if input == "y"
-            list_concerts(input)
-        elsif input == "n"
-            puts ""
-            puts "Thanks for checking out our list of concerts!"
-
-
         puts "To exit, type 'exit'"
         input = gets.strip
 
-
-        case input
-
-        when 'list'
-        self.list_concerts
-
-        when 'exit'
-        puts "Thanks for checking out our list of concerts! Goodbye!"
-        end
+        puts ""
+        puts get_advertised_concerts
+        puts list_concerts
+        input = gets.strip
     end
+    
+        # puts ""
+        # puts "To find out more information about a specific concert, enter the number"
+        # input = gets.strip.to_i
+        # puts 
+
+    #     # puts "Would you like to check out another concert, type 'y' or 'n'?"
+    #     # input = gets.strip.downcase
+    #     # if input == "y"
+    #     #     list_concerts(input)
+    #     # elsif input == "n"
+    #     #     puts ""
+    #     #     puts "Thanks for checking out our list of concerts!"
+
+    #     # else
+    #     puts "To exit, type 'exit'"
+    #     input = gets.strip
+
+    #     case input
+
+    #     when 'list'
+    #     self.list_concerts
+
+    #     else
+    #     puts "Thanks for checking out our list of concerts! Goodbye!"
+    #     end
+    # end
+    # end
+
+    def get_advertised_concerts
+        #to be scraped
+        @concerts = ['Juilliard String Quartet', 'Pacifica Sting Quartet', 'Aragorn String Quartet']
     end
+
+    # def get_user_concert
+    #     #list concerts
+    #     @concerts.each_with_index(1) do |concert, index| 
+    #         puts "#{index}. #{concert}"
+    #     end  
+    # end
 
 
     def list_concerts
         puts "Awesome! Here are a list of concerts"
-        @concerts.each_with_index(1) do |concert, index|
-            puts "#{index}. #{concert.name}"
+        @concerts.each_with_index do |concert, index|
+            puts "#{index + 1}. #{concert.name}"
         end
-        main_menu
-    end
+    #     main_menu
+     end
 
-    def print_concerts(concert)
-        puts ""
-        puts "==========#{concert.name}=========="
-        puts ""
-        puts "Date:         #{concert.date}"
-        puts "Description:  #{concert.description}"
-        puts "Contact:      #{concert.contact}"
-        puts "Hours:      #{brewery.hours}"
-        puts "==============About Us=============="
-        puts "Details:    #{concert.details}" 
-        puts ""
-        main_menu
-    end
+    # def print_concerts(concert)
+    #     puts ""
+    #     puts "==========#{concert.name}=========="
+    #     puts ""
+    #     puts "Date:         #{concert.date}"
+    #     puts "Description:  #{concert.description}"
+    #     puts "Contact:      #{concert.contact}"
+    #     puts "Hours:      #{brewery.hours}"
+    #     puts "==============About Us=============="
+    #     puts "Details:    #{concert.details}" 
+    #     puts ""
+    #     main_menu
+    # end
 
     # puts ""
     #     puts "To find out more information about a specific brewery, enter the number"
@@ -82,6 +105,4 @@ URL = "https://arizonachambermusic.org/tickets/"
 #         print_brewery(brewery)
 
 #         main_menu
-#         end
-  end
-
+end
--- lib/gnakano_cli_project/scraper.rb
+++ lib/gnakano_cli_project/scraper.rb
@@ -8,30 +8,31 @@ class GnakanoCliProject::Scraper
     #parsed it with Nokogiri for each concert
     #I have instantiated an instance of a concert with the corresponding data
   end
+end
       
 
-  def self.scrape_by_index(index_url)
-    binding.pry
-    concerts = []
-      self.load.css("div.post_content").each |concert|
+  # def self.scrape_by_index(index_url)
+  #   binding.pry
+  #   @concerts = []
+  #     self.load.css("div.post_content").each |concert|
 
-      concert_hash  = {}
-      concert_hash[:name] = concert.css("h1.event-title").text
-      concert_hash[:date] = concert.css("h2.event-subtitle").text
-      concert_hash[:description] = concert.css("p").text
+  #     concert_hash  = {}
+  #     concert_hash[:name] = concert.css("h1.event-title").text
+  #     concert_hash[:date] = concert.css("h2.event-subtitle").text
+  #     concert_hash[:description] = concert.css("p").text
 
-      concerts << concert_hash
-      end
-    concerts
-  end
+  #     concerts << concert_hash
+  #     end
+  #   concerts
+  # end
 
-  def self.scrape_concert_page(concert_url)
-    scraped_concert = {}
-      concert = Nokogiri::HTML(open(concert_url))
-      #scraped_concert[:artist_name] = profile.css("h1.event-title").text
-      scraped_concert[:event_time] = profile.css("h2.event-subtitle").text
-      scraped_concert[:program] = profile.css("ul.list-unstyled.list-pieces").text
-      scraped_concert[:bio] = doc.css("div.text-serif").text
-      scraped_concert[:ticket_info] = doc.css("div.panel.text-sans").text
-      scraped_concert
-    end
+  # def self.scrape_concert_page(concert_url)
+  #   scraped_concert = {}
+  #     concert = Nokogiri::HTML(open(concert_url))
+  #     #scraped_concert[:artist_name] = profile.css("h1.event-title").text
+  #     scraped_concert[:event_time] = profile.css("h2.event-subtitle").text
+  #     scraped_concert[:program] = profile.css("ul.list-unstyled.list-pieces").text
+  #     scraped_concert[:bio] = doc.css("div.text-serif").text
+  #     scraped_concert[:ticket_info] = doc.css("div.panel.text-sans").text
+  #     scraped_concert
+  #   end
